a32d487309edcf08cc7b6e1ab555c8927a420565
lotlouischoitslab <louis.s.cho@gmail.com>
2023-01-07 10:17:33 -0600

    add

--- 00PYTHON/04Python_Intro/Sockets/server.py
+++ 00PYTHON/04Python_Intro/Sockets/server.py
@@ -1,10 +1,12 @@
 import socket 
 import threading 
 
+HEADER = 64 #Message with 64 bits
 PORT = 5050
 #SERVER = '172.17.64.1' #run on local network
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDRESS = (SERVER,PORT) #needs to have server and port
+FORMAT = 'UTF-8'
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #(family,type)
 server.bind(ADDRESS)
@@ -14,8 +16,10 @@ def handle_client(connections,address):
 
     connected = True 
     while connected:
-        msg = conn.recv()
-        
+        msg_length= conn.recv(HEADER).decode(FORMAT)
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+        print(f'[{addr}] {msg}')
 
 
 def start():
