a16a0ec812bfb4e9567c1557e6593f5b57f2f17e
lotlouischoitslab <louis.s.cho@gmail.com>
2023-01-07 10:37:50 -0600

    add sockets:

--- 00PYTHON/04Python_Intro/Sockets/client.py
+++ 00PYTHON/04Python_Intro/Sockets/client.py
@@ -4,10 +4,24 @@ HEADER = 64 #Message with 64 bits
 PORT = 5050 #Port
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDRESS = (SERVER,PORT)
+FORMAT = 'UTF-8'
+DISCONNECT_MESSAGE = '!DISCONNECT'
 
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect(ADDRESS) 
 
 def send(msg):
     message = msg.encode(FORMAT)
-    
+    msg_length = len(message) 
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' '*(HEADER - len(send_length)) #byte representation in string
+    client.send(send_length)
+    client.send(message)
+
+send('Hello World!')
+input()
+send('Hello Everyone!')
+input()
+send('Hello Louis Sungwoo Cho!')
+
+send(DISCONNECT_MESSAGE)
