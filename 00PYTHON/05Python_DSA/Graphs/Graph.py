ec4a651dba127a4191fcdafed7ecf01e3ed0e702
lotlouischoitslab <louis.s.cho@gmail.com>
2023-01-09 23:14:21 -0600

    add graph

---
+++ 00PYTHON/05Python_DSA/Graphs/Graph.py
@@ -0,0 +1,46 @@
+class Graph:
+    def __init__(self):
+        self.nodes = []
+        self.graph = dict() #Because this is an empty dictionary, I will assume this to be empty dictionary
+    
+    def __init__(self,nodes):
+        self.nodes = nodes 
+        self.graph = dict()
+
+        #Now I will be adding empty edges!
+        for n in self.nodes:
+            self.graph[n] = []
+
+    #Add edge to the graph network
+    def addedge(self,u,v,weight):
+        self.graph[u].append([v,weight])
+    
+    #Add a node/vertex to the graph network
+    def addnode(self,node):
+        if node not in self.nodes:
+            self.nodes.append(node)
+
+    #Remove a node/vertex in the graph network
+    def removedge(self,u,v,weight):
+        if u in self.graph and v in self.graph:
+            del self.graph[u]
+            for i in self.graph:
+                list1 = self.graph[i]
+                for j in list1:
+                    if node == j[0]:
+                        list1.remove(j)
+                        break
+    
+    #Degree of the node
+    def degree(self,node):
+        return len(self.graph[node])
+
+    #Number of Edges
+    def numedges(self):
+        if self.directed:
+            return (self.numvertices()*(self.numvertices()-1))/2
+        return self.numvertices()*(self.numvertices()-1)
+    
+    #Number of Vertices
+    def numvertices(self):
+        return len(self.graph)
---
+++ 00PYTHON/05Python_DSA/Graphs/GraphsMP.py
@@ -0,0 +1,15 @@
+from Graph import Graph
+
+
+
+
+
+
+
+
+
+def main():
+    print('Graphs MP')
+
+if __name__ == '__main__':
+    main()
